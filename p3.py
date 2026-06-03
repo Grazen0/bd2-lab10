094dbde383f7074b0cc0556c9fe975c8a8d44686
Joaquin Lopez <jadrian.ldc@proton.me>
2026-06-03 16:43:32 -0500

    refactor: read dataframe in p3.py from xlsx instead of arff

--- p3.py
+++ p3.py
@@ -1,7 +1,4 @@
 import pandas as pd
-from scipy.io.arff import loadarff
 
-raw_data = loadarff("DryBeanDataset/Dry_Bean_Dataset.arff")
-df_data = pd.DataFrame(raw_data[0])
-
+df_data = pd.read_excel("DryBeanDataset/Dry_Bean_Dataset.xlsx")
 df_data.to_csv("dry_beans.csv", index=False)
